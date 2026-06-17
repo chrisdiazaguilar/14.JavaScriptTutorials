d4d76f540b77381add2a5bde78dcd5f40fd9fa1d
Chris Diaz <chrisdiaz0641@gmail.com>
2026-06-17 13:10:40 -0400

    Updated JS file

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 // This is my first JavaScript code!
 console.log("Hello World");
+
+let name = 'Mosh';
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+// let firstName = 'Chris';
+// let lastName = 'Diaz';
+
+// let interestRate = 0.3;
+// interestRate = 1;
+// console.log(interestRate);
+
+let name = 'chris';
+let age = 30;
+let isApproved = false;
+let firstName = undefined;
+let selectedColor = null;
