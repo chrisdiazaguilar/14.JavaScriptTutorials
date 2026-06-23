500690807d9509f65052905f38899ba381e0a3fe
Chris Diaz <chrisdiaz0641@gmail.com>
2026-06-23 07:34:21 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-// This is my first JavaScript code!
-console.log("Hello World");
+// // This is my first JavaScript code!
+// console.log("Hello World");
 
-let name = 'Mosh';
-console.log(name);
+// let name = 'Mosh';
+// console.log(name);
 
-// Cannot be a reserved keyword
-// Should be meaningful
-// Cannot start with a number (1name)
-// Cannot contain a space or hyphen (-)
-// Are case-sensitive
+// // Cannot be a reserved keyword
+// // Should be meaningful
+// // Cannot start with a number (1name)
+// // Cannot contain a space or hyphen (-)
+// // Are case-sensitive
 
-// let firstName = 'Chris';
-// let lastName = 'Diaz';
+// // let firstName = 'Chris';
+// // let lastName = 'Diaz';
 
-// let interestRate = 0.3;
-// interestRate = 1;
-// console.log(interestRate);
+// // let interestRate = 0.3;
+// // interestRate = 1;
+// // console.log(interestRate);
 
-let name = 'chris';
-let age = 30;
-let isApproved = false;
-let firstName = undefined;
-let selectedColor = null;
+// // let name = 'chris';
+// // let age = 30;
+// // let isApproved = false;
+// // let firstName = undefined;
+// // let selectedColor = null;
+
+// let persons = {
+//     name: 'Mosh',
+//     age: 30
+// }
+
+// // Dot Notation
+// person.name = 'John';
+
+// // Bracket Notation
+// person['name'] = 'Mary';
+// // let selection = 'name';
+// // person[selection] = 'Mary';
+
+// console.log(person.name);
+
+// let selectedColor = ['red', 'blue'];
+// selectedColors[2] = 1;
+// console.log(selectedColors.length);
+
+// //Functions
+function greet(name) {
+    console.log('Hello ' + name);
+ }
+
+greet('Chris');
+greet('Jose');
+
+//Calculating value
+function square(number) {
+    return number * number;
+}
+
+console.log(square(2));
